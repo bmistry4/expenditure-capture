f1cd3925180092c7c2038c9251a33a7b1ba9f8e4
Bhumika <mistrybhk@gmail.com>
2019-07-25 19:06:25 +0100

    got all basic functionality working including updating expenditure sheet

--- src/model.py
+++ src/model.py
@@ -5,10 +5,12 @@ import pandas as pd
 import xlwings as xw
 from kivy.uix.spinner import Spinner
 from kivy.uix.textinput import TextInput
+import pprint
 
 transactions_path = r'..\data\jan-feb-2018.xlsx'
 category_path = r'..\data\category-mappings.csv'
-sheet = 'transactions'
+budget_path = r'..\data\budget-template.xlsx'
+
 DATE = 'date'
 DESCRIPTION = 'description'
 MONEY_IN = 'money_in'
@@ -16,7 +18,8 @@ MONEY_OUT = 'money_out'
 BALANCE = 'balance'
 CATEGORY = 'category'
 
-START_ROW = '6'
+START_ROW = 6
+DATE_RANGE_CELL = 'D2'
 
 WEEKLY_BUDGET = 100  # TODO - remove hardcode
 CATEGORY_LIST = [
@@ -28,7 +31,7 @@ CATEGORY_LIST = [
     'other',
     'education',
     'travel',
-    'sport',
+    'beauty',
 ]
 
 
@@ -36,9 +39,17 @@ class Model:
 
     def __init__(self):
         self.cat_map = self.read_category_csv()
-        self.transaction_mappings = {}
+        self.transaction_mappings = {}  # todo - make local scope
         self.transactions_df = None
         self.unknown_cat_map = []
+        self.one_off_mappings = []  # one time selections when cat different to saved default
+        self.id_to_df_index = {}  # id on gui table to df index map
+
+        self.expenditure_file = None
+        self.expenditure_sheet = None
+        self.update_column = None  # column corresponding to week to update
+
+        self.beginning_date = None
 
     def read_category_csv(self):
         # read in cat mappings (as dict)
@@ -68,6 +79,24 @@ class Model:
         self.transactions_df = transactions
 
         self.add_inital_category_mappings()
+        self.set_beginning_date(sheet)
+
+    def load_expenditure_file(self):
+        wb = self.load_excel(budget_path)
+        sheet = wb.sheets[1]  # todo - search sheet by name so not index hardcoded
+        self.update_column = self.find_column_to_update(sheet)
+        self.expenditure_sheet = sheet
+
+    def find_column_to_update(self, sheet):
+        # returns column value corresponding to the correct week
+        column = 'A'
+        row = '6'
+        cell = column + row
+        while sheet.range(cell).value is not None:
+            # fixme : limitation - only works till Z then goes to invalid [ not AA
+            column = chr(ord(column) + 1)
+            cell = column + row
+        return column
 
     def create_transactions_mapping(self, sheet):
         # sht.range('A1:A5').options(ndim=2).value # row
@@ -87,6 +116,11 @@ class Model:
             id += 1
             cell = 'B' + str(row)
 
+    def set_beginning_date(self, sheet):
+        ' gets the date of the week beginning'
+        # 01/01/2018 to 01/02/2018 returns 01/01/2018
+        self.beginning_date = sheet.range(DATE_RANGE_CELL).value.split(' to ')[0]
+
     def get_final_balance(self):
         '''Get latest balance'''
         return self.transactions_df.iloc[-1][BALANCE]
@@ -97,16 +131,25 @@ class Model:
     def get_total_in(self):
         return self.transactions_df[MONEY_IN].sum()
 
-    def calc_weekly_savings(self):
-        return WEEKLY_BUDGET - self.get_total_out()
+    def calc_weekly_savings(self, weekly_spendings):
+        ''' Will only consider desp with assoc cats. So please elect transactions not considered as the cause be large
+        transfers which do not want to be considered '''
+        return WEEKLY_BUDGET - weekly_spendings
 
-    def calc_total_savings(self):
-        # accumulative total
-        pass
+    def calc_total_savings(self, weekly_savings):
+        '''return accumulative total'''
+
+        # if first week savings will be the weekly saving for that week
+        if self.update_column is 'B':
+            return weekly_savings
+        else:
+            prev_week_col = chr(ord(self.update_column) - 1)
+            current_total = self.expenditure_sheet.range(prev_week_col + '21').value
+            return float(current_total) + float(weekly_savings)
 
     def get_description_cat(self, description: str):
         for desp, cat in self.cat_map.items():
-            if desp in description:
+            if desp.lower() in description.lower():
                 return cat
 
     def add_inital_category_mappings(self):
@@ -120,37 +163,61 @@ class Model:
                 self.unknown_cat_map.append((row.Index, row_desp))
 
     def calc_category_totals(self):
-        unknown_cat_map = []
-        for row in self.transactions_df.itertuples():
-            row_desp = getattr(row, DESCRIPTION)
-            category = self.get_description_cat(row_desp)
-            # assoc desb with known cat
-            if category:
-                self.transactions_df.iloc[row.Index][CATEGORY] = category
-            else:
-                unknown_cat_map.append((row.Index, row_desp))
-
-        # assoc unknown cats - TODO
-
         # add totals together
         category_totals = {}
         for cat in CATEGORY_LIST:
-            category_totals[cat] = self.transactions_df[self.transactions_df[CATEGORY] == cat][BALANCE].sum()
+            category_totals[cat] = self.transactions_df[self.transactions_df[CATEGORY] == cat][MONEY_OUT].sum()
 
+        pprint.pprint(category_totals)
         return category_totals
 
-    def update_weekly_totals(self, transactions_df):
-        cat_totals = self.calc_category_totals(transactions_df)
-        # update cat totals
+    def update_weekly_totals(self):
+        # updates week cell
+        self.expenditure_sheet.range(self.update_column + '4').value = self.beginning_date
+
+        # updates category totals
+        cat_totals = self.calc_category_totals()
+        row = START_ROW
+        cat_heading_cell = 'A' + str(row)
+        while self.expenditure_sheet.range(cat_heading_cell).value is not None:
+            self.expenditure_sheet.range(self.update_column + str(row)).value = cat_totals[
+                self.expenditure_sheet.range(cat_heading_cell).value]
+            row += 1
+            cat_heading_cell = 'A' + str(row)
 
         # update balance, in, out, weekly savings, total savings
-        pass
+        weekly_spending = sum(cat_totals.values())
+        weekly_savings = self.calc_weekly_savings(weekly_spending)
+        self.expenditure_sheet.range(self.update_column + '19').value = weekly_spending
+        self.expenditure_sheet.range(self.update_column + '20').value = weekly_savings
+        self.expenditure_sheet.range(self.update_column + '21').value = self.calc_total_savings(weekly_savings)
+
+        # balance
+        self.expenditure_sheet.range(self.update_column + '23').value = self.get_final_balance()
+
+        # totals for each category
+        self.update_budget_totals()
 
     def update_budget_totals(self):
+        # TODO - test
         # update budget stuff on right
-        pass
+        cat_row = START_ROW
+        while self.expenditure_sheet.range('A' + str(cat_row)).value is not None:
+            week_col = 'B'
+            cat_total = 0
+
+            cell_value = self.expenditure_sheet.range(week_col + str(cat_row)).value
+            while cell_value is not None:
+                cat_total += float(cell_value)
+                week_col = chr(ord(week_col) + 1)
+                new_cell = week_col + str(cat_row)
+                cell_value = self.expenditure_sheet.range(new_cell).value
+            # update total value for category row
+            self.expenditure_sheet.range('R' + str(cat_row)).value = cat_total
+            cat_row += 1
 
     def add_transactions_rows(self, table):
+        table_index = 0
         for row in self.transactions_df.itertuples():
             # only account for costings, not savings
             if getattr(row, MONEY_OUT) is not None:
@@ -171,6 +238,18 @@ class Model:
                                          # 'color_click': [0.23, 0.24, .5, 1],
                                          'values': CATEGORY_LIST,
                                          }])
+                self.id_to_df_index[table_index] = getattr(row, "Index")
+                table_index += 1
+
+    def update_categories(self, table_cells):
+        # iterate through all cells in gui and update model dataframe
+        for table_id, row in enumerate(table_cells):
+            df_cat = self.transactions_df.iloc[self.id_to_df_index[table_id]][CATEGORY]
+            # if one-off category (i.e. a different category is chosen to the usual one for that desp)
+            # one-offs do not cover transaction desps which have never been seen
+            if df_cat != row[1].text and df_cat is not None:
+                self.one_off_mappings.append(row[1].text)
+            self.transactions_df.iloc[self.id_to_df_index[table_id]][CATEGORY] = row[1].text
 
 
 if __name__ == '__main__':
--- src/view.py
+++ src/view.py
@@ -9,16 +9,18 @@ from kivy.uix.floatlayout import FloatLayout
 
 from model import Model
 
-Config.set("input", "mouse", "mouse, disable_multitouch")
 from kivy.app import App
 from kivy.core.window import Window
 from src.table import Table
+from kivy.lang import Builder
 
 model = Model()
+
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
+
 
 class Root(FloatLayout):
     loadfile = ObjectProperty(None)
@@ -30,11 +32,12 @@ class Root(FloatLayout):
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self.my_table = Table()
         self.my_table.cols = 2
-        self.my_table.label_panel.labels[0].text = "Description" # heading text
-        self.my_table.label_panel.labels[1].text = "Category" # heading text
+        self.my_table.label_panel.labels[0].text = "Description"  # heading text
+        self.my_table.label_panel.labels[1].text = "Category"  # heading text
 
         self.add_widget(self.my_table)
 
+        self.load_table()
 
     def dismiss_popup(self):
         self._popup.dismiss()
@@ -49,11 +52,12 @@ class Root(FloatLayout):
         except IndexError:
             pass
 
-    def show_load(self):
+    def load_table(self):
         #### TODO - temp
-        self.transactions_filepath.text = r'..\data\jan-feb-2018.xlsx'
+        transactions_filepath = r'..\data\jan-feb-2018.xlsx'
+        # self.transactions_filepath.text = r'..\data\jan-feb-2018.xlsx'
         model.process_transactions_file(
-            self.transactions_filepath.text)  # create dataframe with transaction details
+            transactions_filepath)  # create dataframe with transaction details
         model.add_transactions_rows(self.my_table)
 
         #################
@@ -61,6 +65,16 @@ class Root(FloatLayout):
         # self._popup = Popup(title="Load file", content=content,
         #                     size_hint=(0.9, 0.9))
         # self._popup.open()
+
+    def update_budget(self):
+        # pretty much does everything - todo (do better)
+
+        # updates dataframe with latest selection
+        model.update_categories(self.my_table.grid._cells)
+        # calc cat totals and update them on spreadsheet
+        model.load_expenditure_file()
+
+        model.update_weekly_totals()
 
     def _keyboard_closed(self):
         pass
@@ -85,6 +99,7 @@ class Root(FloatLayout):
         if keycode[0] == 279:  # End
             print(keycode)
             self.my_table.scroll_view.end()
+
 
 class ViewApp(App):
     title = "Expenditure capture"
